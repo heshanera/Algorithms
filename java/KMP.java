938232261f8641e969fee8c847928064e8663c87
Heshan Eranga <erangamx@gmail.com>
2016-06-12 16:27:37 +0530

    Add files via upload

---
+++ java/KMP.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+public class KMP
+{
+	public static int[] prefix_func(String ptrn)
+	{
+		int j = 0 ,pos = 1;
+		int ptrnlen = ptrn.length();
+		int[] prfx = new int[ptrnlen];
+		prfx[0] = 0;
+		while (pos < ptrnlen)
+		{
+			
+			while ((j > 0) &&  (ptrn.charAt(j) != ptrn.charAt(pos)))
+			{
+				j = prfx[j-1];
+			}
+			if ( ptrn.charAt(j) == ptrn.charAt(pos) )
+			{
+				j++;
+			}
+			prfx[pos] = j;
+			pos++;
+		}
+	
+		return prfx;
+	}
+	
+	public static int match(String txt, String ptrn)
+	{
+		int[] prfx = prefix_func(ptrn);
+		boolean match_found = false;
+		int txtlen, ptrnlen, i = 0, j = 0;
+		txtlen = txt.length();
+		ptrnlen = ptrn.length();
+		while (i < txtlen)
+		{
+			while ((j > 0) && (ptrn.charAt(j) != txt.charAt(i)))
+			{
+				
+				j = prfx[j];
+			}
+			if (ptrn.charAt(j) == txt.charAt(i))
+			{
+				j++;
+			}
+			if (j == ptrnlen)
+			{
+				System.out.println("match found from " +  (i-ptrnlen+1) + " to " + i);
+				j = prfx[j-1];
+				i += (ptrnlen - 1);
+				match_found = true;
+			}
+			i++;
+		}
+		if (match_found == false)
+		{
+			System.out.println("NO match found!!!"); 
+		}
+		return 0;
+	}
+	
+	public static int matchOverlaping(String txt, String ptrn)
+	{
+		int[] prfx = prefix_func(ptrn);
+		boolean match_found = false;
+		int txtlen, ptrnlen, i = 0, j = 0;
+		txtlen = txt.length();
+		ptrnlen = ptrn.length();
+		while (i < txtlen)
+		{
+			while ((j > 0) && (ptrn.charAt(j) != txt.charAt(i)))
+			{
+				
+				j = prfx[j];
+			}
+			if (ptrn.charAt(j) == txt.charAt(i))
+			{
+				j++;
+			}
+			if (j == ptrnlen)
+			{
+				System.out.println("match found from " +  (i-ptrnlen+1) + " to " + i);
+				j = prfx[j-1];
+				match_found = true;
+			}
+			i++;
+		}
+		if (match_found == false)
+		{
+			System.out.println("NO match found!!!"); 
+		}
+		return 0;
+	}
+	
+	/*
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		String[] inputArray = input.split(" ");
+		String text = inputArray[0];
+		String pattern = inputArray[1];
+		KMP.match(text,pattern);	
+	}
+	*/ 
+	
+}
---
+++ java/RabinCarp.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class RabinCarp
+{	
+	public static long calcPatternHash(String def)
+	{
+		int length = def.length();
+		long sVal = 0;
+		for (int i = 0; i < length; i++)
+		{
+			sVal = ((int)(def.charAt(i)) + sVal)*3;
+		}
+		return sVal;
+	}
+	
+	public static boolean match_deep(String p1, String p2)
+	{
+		int l = p1.length();
+		boolean match = true;
+		for (int i = 0; i < l; i++)
+		{
+			if (p1.charAt(i) != p2.charAt(i))
+			{
+				match = false;
+				break;
+			}
+		}
+		return match;
+	}
+	
+	public static void match(String txt, String ptrn)
+	{
+		
+		long patternHash = calcPatternHash(ptrn);
+		long subStringHash = 0;
+		int patternLength = ptrn.length();
+		int textLength = txt.length();
+		
+		int end = patternLength - 1;
+		
+		for (int i = 0 ; i < (textLength-patternLength + 1); i++,end++)
+		{
+			String subString = txt.substring(i,end+1);
+			subStringHash = calcPatternHash(subString);
+			if (subStringHash == patternHash)
+			{
+				boolean matching = match_deep(subString, ptrn);
+				if (matching)
+				{
+					System.out.println("Match found from " + i + " to " + end);
+					i += (patternLength - 1);
+					end += (patternLength - 1);
+				}
+			}
+			
+		}				 
+	}
+	
+	
+	public static void matchOverlaping(String txt, String ptrn)
+	{
+		
+		long patternHash = calcPatternHash(ptrn);
+		long subStringHash = 0;
+		int patternLength = ptrn.length();
+		int textLength = txt.length();
+		
+		int end = patternLength - 1;
+		
+		for (int i = 0 ; i < (textLength-patternLength + 1); i++,end++)
+		{
+			String subString = txt.substring(i,end+1);
+			subStringHash = calcPatternHash(subString);
+			if (subStringHash == patternHash)
+			{
+				boolean matching = match_deep(subString, ptrn);
+				if (matching)
+				{
+					System.out.println("Match found from " + i + " to " + end);
+				}
+			}
+			
+		}				 
+	}
+	
+	
+	/*
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		String[] inputArray = input.split(" ");
+		String text = inputArray[0];
+		String pattern = inputArray[1];
+		RabinCarp.matchOverlaping(text,pattern);	
+	}
+	*/
+	
+}	
